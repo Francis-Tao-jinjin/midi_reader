df9431990e4a0edf713fb6be0c14d4deb91b539f
Tao Jinjin <taojinjinlive@hotmail.com>
2019-03-29 12:28:00 +0800

    update midifile

--- midifile.js
+++ midifile.js
@@ -11,6 +11,72 @@ function MidiFile(data) {
         }
     }
 
+    var lastEventTypeByte;
+    // http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html#BM2_2
+    function readEvent(stream) {
+        var event = {};
+        event.deltaTime = stream.readVarInt();
+        var eventTypeByte = stream.readInt8();
+        if ((eventTypeByte & 0xf0) == 0xf0) { // System Common Messages and System Real-Time Messages
+            if (eventTypeByte == 0xff) {
+
+            } else if (eventTypeByte == 0xf0) {
+
+            }
+        } else {  // Channel Voice Messages
+            // Data Byte 的第一个 byte
+            var param1;
+            if ((eventTypeByte & 0x80) == 0) {
+                param1 = eventTypeByte;
+                eventTypeByte = lastEventTypeByte;
+            } else {
+                param1 = stream.readInt8();
+                lastEventTypeByte = eventTypeByte;
+            }
+            // 前四位是 event 类型， 后四位是 channel 编号
+            var eventType = eventTypeByte >> 4;
+            event.channel = eventTypeByte & 0x0f;
+            event.type = 'channel';
+            switch (eventType) {
+                case 0x80:
+                    event.subType = 'noteOff';
+                    event.noteNumber = param1;
+                    // dataByte 的第二个字节
+                    event.velocity = stream.readInt8();
+                    return event;
+                case 0x09:
+                    event.noteNumber = param1;
+                    event.velocity = stream.readInt8();
+                    if (event.velocity === 0) {
+                        event.subType = 'noteOff';
+                    } else {
+                        event.subtype = 'noteOn';
+                    }
+                    return event;
+                case 0x0a:
+                    event.subType = 'noteAftertouch';
+                    event.noteNumber = param1;
+                    event.amount = stream.readInt8();
+                    return event;
+                case 0x0b:
+                    event.subtype = 'controller';
+                    event.controllerType = param1;
+                    event.value = stream.readInt8();
+                    return event;
+                case 0x0d:
+                    event.subtype = 'channelAftertouch';
+                    event.amount = param1;
+                    return event;
+                case 0x0e:
+                    event.subType = 'pitchBend';
+                    event.value = param1 + (stream.readInt8() << 7);
+                    return event;
+                default:
+                    throw 'Unrecognised MIDI event type: ' + eventType;
+            }
+        }
+    }
+
     var stream = Stream(data);
     var headerChunk = readChunk(stream);
     if (headerChunk.id != 'MThd' || headerChunk.length != 6) {
@@ -29,4 +95,29 @@ function MidiFile(data) {
     } else {
         ticksPerBeat = timeDivision;
     }
+
+    var header = {
+        formatType,
+        trackCount,
+        ticksPerBeat,
+    };
+
+    var tracks = [];
+    for (var i = 0; i < header.trackCount; i++) {
+        tracks[i] = [];
+        var trackChunk = readChunk(stream);
+        if (trackChunk.id != 'MTrk') {
+            throw "Unexpected chunk - expected MTrk, got " + trackChunk.id;
+        }
+        var trackStream = Stream(trackChunk.data);
+        while (!trackStream.eof()) {
+            var event = readEvent(trackStream);
+            tracks[i].push(event);
+        }
+    }
+
+    return {
+        header,
+        tracks,
+    };
 }
